2556b727aa471f547c9d1d16b95bd456539dba8e
Louis Gérard <louis@gerard.li>
2026-05-04 17:17:59 +0200

    changes

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,4 +1,31 @@
 return {
+  {
     "mason-org/mason.nvim",
-    opts = {}
+    config = true,
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = { "mason-org/mason.nvim" },
+    opts = {
+      ensure_installed = { "basedpyright" },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "saghen/blink.cmp",
+    },
+    config = function()
+      require("mason-lspconfig").setup({
+        handlers = {
+          function(server_name)
+            require("lspconfig")[server_name].setup({
+              capabilities = require("blink.cmp").get_lsp_capabilities(),
+            })
+          end,
+        },
+      })
+    end,
+  },
 }
